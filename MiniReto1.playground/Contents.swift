633962abf924ee42ab9d587e9ee00c8a701bc32c
John L <jlorenzoj@gmail.com>
2015-12-07 11:33:47 -0400

    Juego de Memoria
    Assignment: Juego de Memoria

---
+++ MiniReto1.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+let rango = 30...40
+
+for num in 0...100{
+    if num%5 == 0 {
+        print("#\(num) Bingo!!!")
+    }else if num%2 == 0 {
+        print("#\(num) Par!!!")
+    }
+    else{
+        print("#\(num) Impar!!!")
+    }
+    
+   switch num{
+    case 30...40:
+        print("#\(num) Viva Swift!!!")
+    default : break
+    }
+}
+
+
+
